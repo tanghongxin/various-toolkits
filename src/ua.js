c23bac642aa57a68d4bb92223f1cee768ca3528a
tanghongxin <hongxin.tang@hotmail.com>
2020-10-11 16:54:45 +0800

    ✨ (ua) determine if it is a windows system

--- src/ua.js
+++ src/ua.js
@@ -2,3 +2,8 @@ export function isWX() {
   const ua = navigator.userAgent.toLowerCase();
   return ua.includes('micromessenger');
 }
+
+export function isWin() {
+  const { platform } = navigator
+  return ['Win32', 'Windows'].includes(platform)
+}
